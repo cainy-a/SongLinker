29382ac9f0b712e0350cff97cd87381a275cede1
cainy-a <cainy-a@users.noreply.github.com>
2021-01-08 23:41:56 +0000

    b

---
+++ SongLinker/JsonType.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SongLinker
+{
+	public class JsonType
+	{
+		[JsonPropertyName("pageUrl")]
+		public string PageUrl { get; set; }
+	}
+}
---
+++ SongLinker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace SongLinker
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string url = null;
+			try
+			{
+				url = args[0];
+			}
+			catch (Exception)
+			{
+				// ignored
+			}
+
+			if (url == null)
+			{
+				Console.Write("Enter a URL:");
+				url = Console.ReadLine();
+			}
+
+			Console.WriteLine(GetShortUrl(url));
+		}
+
+		private static string GetShortUrl(string streamingUrl)
+		{
+			var apiUrl = $"http://api.song.link/v1-alpha.1/links?url={streamingUrl}";
+			
+			var client = new WebClient();
+			var json = client.DownloadString(apiUrl);
+			var parsed = (JsonType) JsonSerializer.Deserialize(json, typeof(JsonType), new JsonSerializerOptions());
+			return parsed.PageUrl;
+		}
+	}
+}
